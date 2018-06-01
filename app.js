c6bee4f1f16bdb9620c9e510bf2675018c5fb10e
Kat Miller <kat.e.millermartinez@gmail.com>
2018-06-01 10:28:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-$(document).ready(function(){
-  //$('body').html('<p>HW</p>');
-  $('.submitForm').on('click', function() {
-    let textFieldValue = $('.textField').val();
-    $('.debug').text(textFieldValue);
-  })
-});
+$(document).ready(function() {
+	$('.submitForm').on('click', function() {
+		let textFieldValue = $('.textField').val();
+		$('.debug').text(textFieldValue);
+
+	    localStorage.setItem('myFormTextData', textFieldValue)
+	});
+
+	$('.textField').on('change', function() {
+		let textFieldValue = $('.textField').val();
+		$('.debug').text(textFieldValue);
+	})
+})
 

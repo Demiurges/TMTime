9999dca7147e15563319aeb8a975abad678a84f9
Kat Miller <kat.e.millermartinez@gmail.com>
2018-06-01 10:08:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+$(document).ready(function(){
+  //$('body').html('<p>HW</p>');
+  $('.submitForm').on('click', function() {
+    let textFieldValue = $('.textField').val();
+    $('.debug').text(textFieldValue);
+  })
+});
 

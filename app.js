48e1bccbca4267dd668d99d515adca7d936c387f
demiurges <kat.e.millermartinez@gmail.com>
2018-06-05 22:51:12 -0700

    finally figured out a way of showing the snippets without filtering

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 $(document).ready(function(){
 
-  $('.setData').on('click', function() {
+  $('.set-data').on('click', function() {
     let nameInput = $('#save-name');
     let name = nameInput.val();
-    let snippetInput = $('#snippetContainer');
+    let snippetInput = $('#snippet-container');
     let snippet = snippetInput.val();
+    
+    if(localStorage.getItem(name)){
+      alert(`It seems you already saved a snippet with that name. Please try a different name.`);
+    } else {
+      localStorage.setItem(name, snippet);
+    }
 
-    localStorage.setItem(name, snippet);
     nameInput.val('');
     snippetInput.val('');
   });
 
-  $('.getData').on('click', function(){
-    let retrievedData = localStorage.getItem('myFormTextData');
+  $('.get-data').on('click', function(){
+    $('all-snippets').hide();
+    let getNameInput = $('#get-name');
+    let getName = getNameInput.val();
+    let snippet = localStorage.getItem(getName);
+
+    if(snippet){
+      $('.show-snippet').html(`
+      <div class="specific-snippet">${snippet}</div>
+    `).show();
+    } else {
+      alert(`It seems you didn't save any snippets with that name...`);
+    }
+ ;
+
   });
 
 });
